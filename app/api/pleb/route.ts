152294003bf61dc3fffddb21aa822eaf5e2ff125
nzai <nzaih18@gmail.com>
2023-09-19 17:37:46 +0300

    crying

--- app/api/pleb/route.ts
+++ app/api/pleb/route.ts
@@ -1,22 +1,37 @@
-interface CallbackMetadataItem {
-  Name: string;
-  Value: string | number | Date;
-}
+import { NextRequest, NextResponse } from "next/server";
+// @ts-nocheck
+export async function POST(req: Request, res: Response) {
+  const callbackData = await req.body!;
 
-interface CallbackMetadata {
-  Item: CallbackMetadataItem[];
-}
+  const result_code = callbackData.stkCallback.ResultCode;
+  if (result_code !== 0) {
+    // If the result code is not 0, there was an error
+    const error_message = callbackData.Body.stkCallback.ResultDesc;
+    const response_data = {
+      ResultCode: result_code,
+      ResultDesc: error_message,
+    };
+    return res.json(response_data);
+  }
 
-interface StkCallback {
-  MerchantRequestID: string;
-  CheckoutRequestID: string;
-  ResultCode: number;
-  ResultDesc: string;
-  CallbackMetadata: CallbackMetadata;
-}
+  // If the result code is 0, the transaction was completed
+  const body = req.body!.Body.stkCallback.CallbackMetadata;
 
-interface CallbackBody {
-  stkCallback: StkCallback;
-}
+  // Get amount
+  const amountObj = body.Item.find((obj) => obj.Name === "Amount");
+  const amount = amountObj.Value;
 
-export async function POST() {}
+  // Get Mpesa code
+  const codeObj = body.Item.find((obj) => obj.Name === "MpesaReceiptNumber");
+  const mpesaCode = codeObj.Value;
+
+  // Get phone number
+  const phoneNumberObj = body.Item.find((obj) => obj.Name === "PhoneNumber");
+  const phone = phoneNumberObj.Value;
+
+  // Save the variables to a file or database, etc.
+  // ...
+
+  // Return a success response to mpesa
+  return NextResponse.json({ amount, mpesaCode, phone });
+}
