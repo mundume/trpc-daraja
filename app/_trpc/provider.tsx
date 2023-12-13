86a50586899d0e032c11f08511273d75283bf8ef
nzai <nzaih18@gmail.com>
2023-12-13 20:32:16 +0300

    fix some stuff

--- app/_trpc/provider.tsx
+++ app/_trpc/provider.tsx
@@ -12,6 +12,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         httpLink({
+          //
           url: absoluteUrl("/api/trpc"),
         }),
       ],
